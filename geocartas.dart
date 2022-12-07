68f9fb7194ddf5ec8210ab0a49d9c9ecb9fa39aa
devv-thiago <tfofano3@gmail.com>
2022-12-07 08:46:01 -0300

    segundo commit

--- geocartas.dart
+++ geocartas.dart
@@ -2,12 +2,18 @@ import 'dart:io';
 import 'dart:math';
 
 main() {
-  GeoCartas().jogadorJoga();
+  GeoCartas().iniciarGame();
 }
 
 class GeoCartas {
+  int pontuacaoMaquina = 0;
+  int pontuacaoJogador = 0;
+
+  iniciarGame() {
+    jogadorJoga();
+  }
+
   jogadorJoga() {
-    int pontuacaoJogador = 0;
     while (pontuacaoJogador != 5) {
       stdout.write("Digite sua primeira escolha: ");
       String? input1 = stdin.readLineSync();
@@ -17,8 +23,7 @@ class GeoCartas {
       int jogada2 = int.parse(input2 ?? "0");
       if (jogada1 == 1 && jogada2 == 1) {
         // ajustar, somente para teste
-        pontuacaoJogador += 1;
-        print("Pontuação do jogador: $pontuacaoJogador");
+        registraPontuacao(1);
       } else {
         maquinaJoga();
       }
@@ -26,18 +31,27 @@ class GeoCartas {
   }
 
   maquinaJoga() {
-    int pontuacaoMaquina = 0;
     while (pontuacaoMaquina != 2) {
       int jogadaMaquina1 = Random().nextInt(2);
       int jogadaMaquina2 = Random().nextInt(2);
       print("número 1 da máquina: $jogadaMaquina1");
       print("número 2 da máquina: $jogadaMaquina2");
       if (jogadaMaquina1 == 1 && jogadaMaquina2 == 1) {
-        pontuacaoMaquina += 1;
-        print("Pontuação da Máquina: $pontuacaoMaquina");
+        registraPontuacao(2);
       } else {
         jogadorJoga();
       }
+    }
+  }
+
+  registraPontuacao(int quemPontua) {
+    if (quemPontua == 1) {
+      pontuacaoJogador += 1;
+      print("pontuação jogador: $pontuacaoJogador");
+    }
+    if (quemPontua == 2) {
+      pontuacaoMaquina += 1;
+      print("pontuação máquina: $pontuacaoMaquina");
     }
   }
 }
